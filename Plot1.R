ed287297222a22b51736574cbb8213503488b04a
Fernandez Cortes <richard.fernandez.cortes@acciona.com>
2016-05-26 22:16:54 +0200

    add files

---
+++ Plot1.R
@@ -0,0 +1,30 @@
+## download file if not exist
+if (!file.exists("powerconsumption.zip")){
+  uri<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(uri,destfile = "powerconsumption.zip")
+}
+## unzip file
+unzip("powerconsumption.zip")
+
+## load data frame from working directory
+
+powerdata<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+
+## create DateTime field from time and date
+powerdata$DateTime <-paste(powerdata$Date," ",powerdata$Time)
+powerdata$DateTime <- as.POSIXlt(powerdata$DateTime,format="%d/%m/%Y %H:%M:%S")
+
+
+## filter data to only 2007-02-01 and 2007-02-02
+from <- as.POSIXlt("2007-02-01 00:00:00",format="%Y-%m-%d %H:%M:%S")
+to <- as.POSIXlt("2007-02-03 00:00:00",format="%Y-%m-%d %H:%M:%S")
+
+powerdata<-powerdata[powerdata$DateTime>=from & powerdata$DateTime<to,]
+
+##create the graph to a png
+
+png(filename = "plot1.png",width = 480,height = 480)
+
+hist(powerdata$Global_active_power,col="red",main = "Global Active Power",xlab = "Global Active Power (kilowats)")
+
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,45 @@
+## download file if not exist
+if (!file.exists("powerconsumption.zip")){
+  uri<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(uri,destfile = "powerconsumption.zip")
+}
+## unzip file
+unzip("powerconsumption.zip")
+
+## load data frame from working directory
+
+powerdata<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+
+## create DateTime field from time and date
+powerdata$DateTime <-paste(powerdata$Date," ",powerdata$Time)
+powerdata$DateTime <- as.POSIXlt(powerdata$DateTime,format="%d/%m/%Y %H:%M:%S")
+
+
+## filter data to only 2007-02-01 and 2007-02-02
+from <- as.POSIXlt("2007-02-01 00:00:00",format="%Y-%m-%d %H:%M:%S")
+to <- as.POSIXlt("2007-02-03 00:00:00",format="%Y-%m-%d %H:%M:%S")
+
+powerdata<-powerdata[powerdata$DateTime>=from & powerdata$DateTime<to,]
+
+##create the graph to a png
+
+png(filename = "plot4.png",width = 480,height = 480)
+
+## set par mfrows to 2 rows and 2 columns
+
+par(mfrow=c(2,2))
+
+plot(x=powerdata$DateTime,y=powerdata$Global_active_power,type="l",ylab = "Global Active Power",xlab = "")
+
+plot(x=powerdata$DateTime,y=powerdata$Voltage,type="l",ylab = "Voltage",xlab = "datetime")
+
+
+plot(x=powerdata$DateTime,y=powerdata$Sub_metering_1,col="black",type="l",ylab = "Energy sub metering",xlab = "")
+lines(x=powerdata$DateTime,y=powerdata$Sub_metering_2,col="red",type="l")
+lines(x=powerdata$DateTime,y=powerdata$Sub_metering_3,col="blue",type="l")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"),lwd = c(2,2,2),)
+
+plot(x=powerdata$DateTime,y=powerdata$Global_reactive_power,type="l",xlab = "datetime")
+
+
+dev.off()
